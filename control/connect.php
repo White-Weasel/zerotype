292c4a3241cd98e28fe3febb1e0312c6f065db04
Magic-cat-No765 <Magic-cat-No765@users.noreply.github.com>
2022-01-07 08:06:00 +0700

    update connect.php to use clear_db

--- control/connect.php
+++ control/connect.php
@@ -1,8 +1,21 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass="";
-$database = "ltmt-th";
-$conn = mysqli_connect($server, $user, $pass, $database);
-mysqli_query($conn, 'set names "utf8');
+if (isset($_ENV['CLEARDB_DATABASE_URL']))
+{
+    $db_url = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+    $db_server = $db_url["host"];
+    $db_username = $db_url["user"];
+    $db_password = $db_url["pass"];
+    $db = substr($db_url["path"], 1);
+
+    $conn = mysqli_connect($db_server, $db_username, $db_password, $db);
+}
+else
+{
+    $server = "localhost";
+    $user = "root";
+    $pass="";
+    $database = "ltmt-th";
+    $conn = mysqli_connect($server, $user, $pass, $database);
+    mysqli_query($conn, 'set names "utf8');
+}
 ?>
